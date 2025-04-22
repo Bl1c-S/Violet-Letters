90c4b2ef5c57ad623c995f4b4e6176a8b93bea8b
Олександр Кубарич <kuseas5@gmail.com>
2025-04-22 16:56:33 +0300

    Refactor token verification and update dependencies
    Replaced inline token verification logic with a modular `apiTokenVerification` function for improved readability and reusability. Updated `next` and related dependencies to version 15.2.4, along with other minor dependency updates. Adjusted `tsconfig.json` formatting and added `ES2017` as the target for compatibility.

--- app/api/authorization/verify/route.ts
+++ app/api/authorization/verify/route.ts
@@ -1,17 +1,6 @@
-import { NextRequest, NextResponse } from "next/server";
-import { validateToken } from "@/backend/jwt/jwt";
+import { NextRequest } from "next/server";
+import { apiTokenVerification } from "@/app/api/authorization/apiTokenVerifycation";
 
 export default async function POST(request: NextRequest) {
-  const authHeader = request.headers.get("authorization");
-  if (!authHeader) {
-    return NextResponse.json({ message: "Токен отсутствует" }, { status: 401 });
-  }
-  const token = authHeader.split(" ")[1];
-
-  try {
-    const username = await validateToken(token);
-    return Response.json({ username }, { status: 200 });
-  } catch (err) {
-    return Response.json({ error: err }, { status: 401 });
-  }
+  return await apiTokenVerification(request);
 }
--- app/api/users/get/route.ts
+++ app/api/users/get/route.ts
@@ -1,30 +1,33 @@
 import { NextRequest, NextResponse } from "next/server";
 import { DbUser, getUser, ParsToUser } from "@/backend/database/userDbManager";
-import { validateToken } from "@/backend/jwt/jwt";
+import { apiTokenVerification } from "@/app/api/authorization/apiTokenVerifycation";
 
 export async function GET(request: NextRequest) {
-  const authHeader = request.headers.get("authorization");
-  if (!authHeader) {
-    return NextResponse.json({ status: "Токен отсутствует" }, { status: 401 });
+  const response = await apiTokenVerification(request);
+
+  if (response.status !== 200) {
+    return response;
   }
-  const token = authHeader.split(" ")[1];
+
   try {
-    const decodedData = await validateToken(token);
-    console.log(`Попытка получения пользователя: ${decodedData.username}`);
-    const dbUser: DbUser | null = await getUser(decodedData.username);
+    const { username } = await response.json();
+
+    console.log(`Попытка получения пользователя: ${username}`);
+    const dbUser: DbUser | null = await getUser(username);
 
     if (dbUser) {
       const user = ParsToUser(dbUser);
       return NextResponse.json({ user });
     } else {
       return NextResponse.json(
-        { status: `Пользователь: ${decodedData.username} не найден` },
+        { status: `Пользователь: ${username} не найден` },
         { status: 404 },
       );
     }
   } catch (err) {
+    console.error("Error getting user:", err);
     return NextResponse.json(
-      { error: `Ошибка верификации токена` },
+      { error: `Ошибка получения пользователя` },
       { status: 500 },
     );
   }
